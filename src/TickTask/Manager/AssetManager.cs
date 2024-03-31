18c9d3c1e02e42cda750fe12dd618a087d9465be
ryouji <helloryouji@outlook.com>
2024-04-01 00:17:32 +0800

    add AssetManager

---
+++ src/TickTask/Manager/AssetManager.cs
@@ -0,0 +1,19 @@
+namespace TickTask;
+
+public class AssetManager
+{
+    private static string ConfigFolderPath = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+        "TickTask"
+        );
+
+    private static void Load()
+    {
+
+    }
+
+    private static void Save()
+    {
+
+    }
+}
